a0e17ab1a7362f1ccb64006ddce9f68ddf5fc16e
Tesnim19 <tesnimabdi67@gmail.com>
2024-02-05 18:04:55 +0300

    config file

---
+++ Mend/Mend/social_login/config-files/config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Basic Site Settings and API Configuration
+ */
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', '[YOUR USERNAME]');
+define('DB_PASSWORD', '[YOUR PASSWORD, IF ANY]');
+define('DB_NAME', '[YOUR DATABASE NAME]');
+
+// Google API configuration
+define('GOOGLE_CLIENT_ID', '[YOUR ID]');
+define('GOOGLE_CLIENT_SECRET', '[YOUR SECRET]');
+define('GOOGLE_REDIRECT_URL', 'http://localhost/social_login/index.php');
