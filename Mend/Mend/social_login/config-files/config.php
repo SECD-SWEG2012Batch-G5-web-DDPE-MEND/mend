9ecc44611316277dd55313bae43589a05788ab2d
Tensae <tensaeb2016@gmail.com>
2024-02-05 18:29:41 +0300

    google-credentials

--- Mend/Mend/social_login/config-files/config.php
+++ Mend/Mend/social_login/config-files/config.php
@@ -4,11 +4,11 @@
  */
 // Database configuration
 define('DB_HOST', 'localhost');
-define('DB_USERNAME', '[YOUR USERNAME]');
-define('DB_PASSWORD', '[YOUR PASSWORD, IF ANY]');
-define('DB_NAME', '[YOUR DATABASE NAME]');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'menddatabase');
 
 // Google API configuration
-define('GOOGLE_CLIENT_ID', '[YOUR ID]');
-define('GOOGLE_CLIENT_SECRET', '[YOUR SECRET]');
+define('GOOGLE_CLIENT_ID', '850806871887-8ko0lc9fhmtpkp4k3cnao0j2ffsgbld5.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
 define('GOOGLE_REDIRECT_URL', 'http://localhost/social_login/index.php');
